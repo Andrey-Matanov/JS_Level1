0bab049970f5a890ac3f54720eb8888cacff17fa
Andrew Matanov <60537186+Andrey-Matanov@users.noreply.github.com>
2020-07-31 15:50:58 +0500

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -99,18 +99,22 @@ function power(val, pow) {
     }
 }
 
-// Моя версия игры "Угадай число".
+// // Моя версия игры "Угадай число".
 
 // // Практикум. Угадай число
 // let randomNumber = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
 // let guessAmount = 0;
 
-// alert("Компьютер загадал число. Попробуйте его отгадать!");
+// alert("Компьютер загадал число от 1000 до 9999. Попробуйте его отгадать! Если вы хотите завершить игру, нажмите ESC.");
+
 // while (true) {
 //     let nextGuess = prompt("Количество попыток: " + guessAmount + "\nВведите число: ");
 //     if (nextGuess == randomNumber) {
 //         alert("Правильно! Вы угадали число с " + ++guessAmount + " попытки!");
 //         break;
+//     } else if (nextGuess == null) {
+//       alert("Вы завершили игру");
+//       break;
 //     } else if (nextGuess < randomNumber) {
 //         alert("Загаданное число - больше!");
 //     } else {
